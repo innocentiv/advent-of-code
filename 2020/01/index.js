42f0a2c1d937a95978dffb683b46455bf8d7b5a3
Valerio Innocenti Sedili <info@vinnocenti.com>
2020-12-08 19:51:54 +0100

    remove unnecessary console.time

--- 2020/01/index.js
+++ 2020/01/index.js
@@ -22,13 +22,11 @@ firstPart();
 const secondPart = async () => {
   const target = 2020;
   const input = await getInput();
-  console.time("find");
 
   for (const a of input) {
     const [b, c] = findCoupleToTarget(input, target - a);
     if (b) {
       console.log("second part:", a * b * c);
-      console.timeEnd("find");
       break;
     }
   }
